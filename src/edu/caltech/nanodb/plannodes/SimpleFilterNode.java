a87400aa7d3aacfe8c45c613ef909de0af629bd6
Aw Young Qingzhuo <hello@veniversum.me>
2018-01-29 13:09:24 -0800

    Add plan costing for SimpleFilterNode

--- src/edu/caltech/nanodb/plannodes/SimpleFilterNode.java
+++ src/edu/caltech/nanodb/plannodes/SimpleFilterNode.java
@@ -118,8 +118,18 @@ public class SimpleFilterNode extends SelectNode {
         schema = leftChild.getSchema();
         ArrayList<ColumnStats> childStats = leftChild.getStats();
 
-        // TODO:  Compute the cost of the plan node!
-        cost = null;
+        // Estimate plan cost based on child's cost
+        PlanCost childCost = leftChild.getCost();
+        if (childCost != null) {
+            cost = new PlanCost(childCost);
+
+            final float selectivity = SelectivityEstimator.estimateSelectivity(predicate, schema, childStats);
+            cost.cpuCost += cost.numTuples;
+            cost.numTuples *= selectivity;
+        } else {
+            logger.info(
+                    "Child's cost not available; not computing this node's cost.");
+        }
 
         // NOTE:  Normally we would also update the table statistics based on
         //        the predicate, but that's too complicated, so we'll leave
