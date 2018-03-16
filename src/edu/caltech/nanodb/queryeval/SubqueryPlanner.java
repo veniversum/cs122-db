55d2ecbe097646cca1c0ab91fff83aeba9048310
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2018-03-16 16:54:04 -0700

    Add javadoc to SubqueryPlanner

--- src/edu/caltech/nanodb/queryeval/SubqueryPlanner.java
+++ src/edu/caltech/nanodb/queryeval/SubqueryPlanner.java
@@ -12,14 +12,34 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ *
+ */
 public class SubqueryPlanner {
 
+    /**
+     * A reference to a parent planner implementation used to generate subquery
+     * plans.
+     */
     private AbstractPlannerImpl planner;
 
+    /**
+     * An instance of a subquery expression processor used to discover
+     * subqueries in expressions.
+     */
     private SubqueryExpressionProcessor subExprProc;
 
+    /**
+     * A collection of enclosing selects from the query that own this
+     * SubqueryPlanner.
+     */
     private List<SelectClause> enclosingSelectsForSubqueries;
 
+    /**
+     *
+     * @param planner
+     * @param enclosingSelectsForSubqueries
+     */
     public SubqueryPlanner(AbstractPlannerImpl planner,
                            List<SelectClause> enclosingSelectsForSubqueries) {
         this.planner = planner;
@@ -27,6 +47,13 @@ public class SubqueryPlanner {
         this.subExprProc = new SubqueryExpressionProcessor();
     }
 
+    /**
+     * Extracts all subqueries from the supplied Expression and returns them
+     * as a list.
+     *
+     * @param expression Expression that subqueries will be extracted from
+     * @return List subqueries extracted from the specified expression
+     */
     public List<SubqueryOperator> extractSubqueries(Expression expression) {
         expression.traverse(this.subExprProc);
         List<SubqueryOperator> subOps = subExprProc.getSubqueryExpressions();
@@ -34,12 +61,39 @@ public class SubqueryPlanner {
         return subOps;
     }
 
+    /**
+     * Traverses the supplied Expression instance to identify subqueries. A
+     * plan is generated for each of these subqueries. This method also
+     * generates a new Environment instance, which is assigned as the parent
+     * environment to every plan node in identified subqueries.
+     *
+     * @param expression Expression that will be traversed to identify
+     *                   subqueries.
+     * @return Returns Environment instance that was assigned to generated
+     * plan nodes if there was at least one subquery found, or null otherwise
+     * @throws IOException
+     */
     public Environment planSubqueries(Expression expression)
             throws IOException {
         Environment environment = new Environment();
         return planSubqueries(expression, environment);
     }
 
+    /**
+     * Traverses the supplied Expression instance to identify subqueries. A
+     * plan is generated for each of these subqueries. This method use the
+     * supplied Environment instance, which is assigned as the parent
+     * environment to every plan node in identified subqueries.
+     *
+     * @param expression Expression that will be traversed to identify
+     *                   subqueries.
+     * @param environment Environment instance that will be assigned to found
+     *                   subqueries
+     *
+     * @return Returns Environment instance that was assigned to generated
+     * plan nodes if there was at least one subquery found, or null otherwise
+     * @throws IOException
+     */
     public Environment planSubqueries(Expression expression,
                                       Environment environment)
             throws IOException {
@@ -54,6 +108,12 @@ public class SubqueryPlanner {
         else return environment;
     }
 
+    /**
+     * Checks if the supplied collection of expressions contains at least one
+     * subquery in any of the expressions.
+     * @param expressions
+     * @return
+     */
     public boolean containsSubqueries(Collection<Expression> expressions) {
         for (Expression expr : expressions) {
             if (containsSubqueries(expr)) return true;
@@ -61,6 +121,11 @@ public class SubqueryPlanner {
         return false;
     }
 
+    /**
+     * Checks if the supplied expression contains any subqueries.
+     * @param expression
+     * @return
+     */
     public boolean containsSubqueries(Expression expression) {
         if (expression == null) return false;
         expression.traverse(subExprProc);
